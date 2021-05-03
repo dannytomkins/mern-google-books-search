6efe1e15de11f6e72999d8a557675293f52308b6
Danny Tomkins <tomkinsdan@gmail.com>
2021-05-03 15:02:00 -0500

    useState to display saved books, need to convert to component

--- client/src/components/Card/index.js
+++ client/src/components/Card/index.js
@@ -1,22 +1,19 @@
 function Card(props) {
     return (
         <div class="card" >
-           
             <div class="card-body">
-                 <div className="row">
+                <div className="row">
                     <div className="col-sm-2">
-                    <img class="card-img-top" src={props.book.volumeInfo.imageLinks ?  props.book.volumeInfo.imageLinks.thumbnail : "https://lh3.googleusercontent.com/proxy/b9i__UFsIY_s0-ZzOMk4xiWOtEvKPnYu_XIKdLZTJLwoGEiCpusHMukhSksOlU8B2O_d9o_I6CCld8_zr85EScSSh8I32RnJmO-fxvqiLJzbPKoAKcainbuUcIhCTgpXbw" } alt="Card image cap" />
+                        <img class="card-img-top" src={props.book.volumeInfo.imageLinks ? props.book.volumeInfo.imageLinks.thumbnail : "https://lh3.googleusercontent.com/proxy/b9i__UFsIY_s0-ZzOMk4xiWOtEvKPnYu_XIKdLZTJLwoGEiCpusHMukhSksOlU8B2O_d9o_I6CCld8_zr85EScSSh8I32RnJmO-fxvqiLJzbPKoAKcainbuUcIhCTgpXbw"} alt="Card image cap" />
                     </div>
                     <div className="col-sm-10">
-                    <h5 class="card-title">{props.book.volumeInfo.title}</h5>
-                    <h6>{props.book.volumeInfo.authors}</h6>
-                <p class="card-text">{props.book.volumeInfo.description}</p>
-                <a href={props.book.volumeInfo.infoLink} class="btn btn-primary">More info</a>
-                 <button onClick ={()=>{props.handleSaveBook(props.book)}} class="btn btn-primary">Save to Favorites</button>
+                        <h5 class="card-title">{props.book.volumeInfo.title}</h5>
+                        <h6>{props.book.volumeInfo.authors}</h6>
+                        <p class="card-text">{props.book.volumeInfo.description}</p>
+                        <a href={props.book.volumeInfo.infoLink} class="btn btn-primary">More info</a>
+                        <button onClick={() => { props.handleSaveBook(props.book) }} class="btn btn-primary">Save to Favorites</button>
                     </div>
-                 </div>
-
-             
+                </div>
             </div>
         </div>
     )
--- client/src/pages/Saved.js
+++ client/src/pages/Saved.js
@@ -1,7 +1,46 @@
-function Save() {
+import React, { useState, useEffect } from "react";
+
+import api from '../utils/api'
+import Card from '../components/Card'
+
+function Saved() {
+
+    const [books, setBooks] = useState([])
+
+    useEffect(() => {
+        api.getSavedBooks()
+        .then(books => 
+            setBooks(books.data)
+          )
+          .catch(err => console.log(err));
+      }, [])
+
     return (
+        <>
         <p>SAVED</p>
+        <div className="row">
+        {books.length > 0 ? books.map(book =>{
+            return (
+                <div class="card" >
+                <div class="card-body">
+                    <div className="row">
+                        <div className="col-sm-2">
+                            <img class="card-img-top" src={book.image ? book.image : "https://lh3.googleusercontent.com/proxy/b9i__UFsIY_s0-ZzOMk4xiWOtEvKPnYu_XIKdLZTJLwoGEiCpusHMukhSksOlU8B2O_d9o_I6CCld8_zr85EScSSh8I32RnJmO-fxvqiLJzbPKoAKcainbuUcIhCTgpXbw"} alt="Card image cap" />
+                        </div>
+                        <div className="col-sm-10">
+                            <h5 class="card-title">{book.title}</h5>
+                            <h6>{book.authors}</h6>
+                            <p class="card-text">{book.description}</p>
+                            <a href={book.link} class="btn btn-primary">More info</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            )
+        }) : "No books saved." }
+        </div>
+        </>
     )
 }
 
-export default Save
+export default Saved
